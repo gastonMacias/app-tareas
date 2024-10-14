937f1b1b50888b226d41b3e82bd6505f3bc3e06c
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-14 17:56:51 -0300

    se agregaron las funciones para agregarTarea y eliminar tarea

--- src/componentes/base/Base.jsx
+++ src/componentes/base/Base.jsx
@@ -1,14 +1,34 @@
 import IngresoTarea from "../ingresoTarea/IngresoTarea";
 import Tarea from "../tarea/Tarea";
+import { useState } from "react";
 import "./base.css";
 
 const Base = () => {
+
+    const [tareas, setTareas] = useState([]);
+
+    const agregarTarea = (nuevaTarea) => {
+        setTareas([...tareas, nuevaTarea])
+    }
+
+    const handleClose = (index) => {
+        const nuevasTareas = tareas.filter((_, i) => i !== index); // Filtra las tareas, eliminando la que corresponde al índice
+        setTareas(nuevasTareas); // Actualiza el estado con las tareas restantes
+      };
+
+
     return (
         <div className="base">
-            <IngresoTarea />
-            <Tarea />
-            <Tarea />
-            <Tarea />
+            <IngresoTarea agregarTarea={agregarTarea} />
+            <div>
+                {tareas.map((tarea, index) => (
+                    <Tarea key={index}
+                        texto={tarea}
+                        onClose={() => handleClose(index)}
+                    />
+                ))}
+            </div>
+
         </div>
     )
 }
