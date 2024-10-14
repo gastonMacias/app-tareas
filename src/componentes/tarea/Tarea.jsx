d7869cc7a7b55572e1fa092f82f29ecb4e6bab14
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-14 16:23:00 -0300

    componente tarea

---
+++ src/componentes/tarea/Tarea.jsx
@@ -0,0 +1,12 @@
+import "./tarea.css";
+
+const Tarea = () => {
+    return (
+        <div className="tarea">
+            <h3>Tarea a realizar</h3>
+            <button className="cerrarTarea">X</button>
+        </div>
+    )
+}
+
+export default Tarea;
