bd631a459c11ac73216e49736193f9b010cd12e5
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-14 16:23:23 -0300

    componente ingreso tarea (input)

---
+++ src/componentes/ingresoTarea/IngresoTarea.jsx
@@ -0,0 +1,14 @@
+import "./ingresoTarea.css";
+
+const IngresoTarea = () => {
+    return (
+        <form>
+            <input type="text"
+                placeholder="Ingrese la tarea" 
+                className="input"
+                />
+        </form>
+    )
+}
+
+export default IngresoTarea;
