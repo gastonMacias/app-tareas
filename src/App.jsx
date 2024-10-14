a54c15dc88b2cc4fc760a90ed2bd75ad8a7ebfc9
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-14 16:24:29 -0300

    se agregaron los componente header y base

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 
 import './App.css'
+import Base from './componentes/base/Base'
+import Header from './componentes/header/Header'
+
+
 
 function App() {
-  
+
 
   return (
     <>
-      
+      <div className='app'>
+        <Header />
+
+        <Base />
+
+
+      </div>
     </>
   )
 }
