5866a181e059c58ad96fb30857249f747bde0e56
gastonMacias <gastonezequielmacias@gmail.com>
2024-10-14 16:23:36 -0300

    header

---
+++ src/componentes/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css";
+
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <h1>App de Tareas</h1>
+        </header>
+    )
+}
+
+
+export default Header;
